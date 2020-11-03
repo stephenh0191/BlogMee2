d5671c6860ef77e7b69b47031c22ad4a44dba1d8
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-03 12:33:24 +0000

    Add one to many asssocations between users nad aritcles from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,8 @@ class ArticlesController < ApplicationController
  	end
 
     def create
-    	@article = Article.new(article_params)
+		@article = Article.new(article_params)
+		@article.user = User.first
 	    if @article.save
 	    	flash[:notice] = "Article was created successfully."
 	    	redirect_to @article
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_112756) do
+ActiveRecord::Schema.define(version: 2020_11_03_122032) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
