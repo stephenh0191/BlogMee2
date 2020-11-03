6103b79a21dd1901b83d2ee26e2d3cb49d55f355
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-03 16:29:15 +0000

    add_password_digest_to_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
     has_many :articles
     validates :username, presence: true, 
                     uniqueness: {case_sensitive: false}, 
@@ -8,5 +9,6 @@ class User < ApplicationRecord
                     uniqueness: {case_sensitive: false}, 
                     length: {maximum: 105},
                     format: {with: VALID_EMAIL_REGEX}
+has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_122032) do
+ActiveRecord::Schema.define(version: 2020_11_03_155128) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_03_122032) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
