5383f2cd993c0145358e1ee2fd2f7493a73e0bbb
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-04 16:28:21 +0000

    showuserprofileimage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,41 @@
 class UsersController < ApplicationController
 
+    def show
+      @user =User.find(params[:id])
+      @articles = @user.articles 
+    end
+
     def new
         @user = User.new
+    end
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was successfully updated"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to Blogmee #{@user.username}, you have successfully signed up"
-            redirect_to articles_path
+          flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+          redirect_to articles_path
         else
-            render "new"
+          render 'new'
         end
     end
 
-
-    private 
-    def user_params
+     
+    
+      private
+      def user_params
         params.require(:user).permit(:username, :email, :password)
-    end
-
-end
+      end
+ end
