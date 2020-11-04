12f171d8897d75cedf6da4118d3d33772a6a2296
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-04 17:04:12 +0000

    add page to display users and their articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
       @user =User.find(params[:id])
       @articles = @user.articles 
     end
+    def index
+      @users = User.all
+    end
 
     def new
         @user = User.new
