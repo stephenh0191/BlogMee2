4e2bc5ab4f3ad5d03c76bb4d51d1dbb835da2363
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-23 20:33:04 +0000

    restrict actions for users controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,6 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:show, :edit, :update, :destroy]
 	before_action :require_user, expect: [:show, :index]
 	before_action :require_same_user, only: [:edit, :update, :destroy]
-	
     def show
     end
 
