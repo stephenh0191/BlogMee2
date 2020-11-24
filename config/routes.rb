aaae04417710a32eca2ca15de6607bfdf547b2b3
stephenh0191 <stephenhughes0191@gmail.com>
2020-11-24 21:13:59 +0000

    Added Admin role to user, delete aritlces and users

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 	get "about" , to: 'pages#about'
 	resources :articles
 	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
 	get 'login', to: 'sessions#new'
 	post 'login', to: 'sessions#create'
 	delete 'login', to: 'sessions#destroy'
-	resources :users, except: [:new]
 
 end
