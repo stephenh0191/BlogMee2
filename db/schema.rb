737c7f4b0bee3b7ea72572988e9cc2b671dd246e
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-11 20:50:18 +0000

    back-end implementation many to many associations Create new migration file to add article_categories table with category_id and article_id columns. - Create article_category.rb model file and interact with the article_categories table. - Add the many-to-many association code using the relevant has_many through: association helpers in the article, category and article_category models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_200223) do
+ActiveRecord::Schema.define(version: 2021_03_11_201647) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
