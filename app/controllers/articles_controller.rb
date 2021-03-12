270625383d1ea527864e890e71ee9aec9b6c8a32
stephenh0191 <stephenhughes0191@gmail.com>
2021-03-12 19:56:35 +0000

    White-list category ids in article_params method of the articles controller. - Add select box for categories to _form.html.erb (form partial) used for articles (views/articles/_form.html.erb)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,8 +50,8 @@ class ArticlesController < ApplicationController
 	end
 
 	def article_params
-		params.require(:article).permit(:title, :description)
-	end
+ 	 params.require(:article).permit(:title, :description, category_ids: [])
+  	end
 
 	def require_same_user
 		if current_user != @article.user && !current_user.admin?
